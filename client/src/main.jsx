550db79458efc909e2a681eb705c23e4d2eccf31
tframe98 <tylerframe8@gmail.com>
2025-07-11 13:19:42 -0700

    .

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -18,7 +18,7 @@ if (!PUBLISHABLE_KEY) {
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ClerkProvider publishableKey={PUBLISHABLE_KEY} afterSignOutUrl='/'>
+    <ClerkProvider publishableKey={clerkPubKey} afterSignOutUrl='/'>
       <App />
     </ClerkProvider>
   </React.StrictMode>,
